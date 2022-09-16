06bd51b23b1cd95c8c6abc07baab877c9718d869
Dylan King <dwkblue51@yahoo.com>
2022-09-16 16:25:50 -0500

    add item detail controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -15,3 +15,18 @@ exports.itemsList = function (req, res, next) {
         });
     })
 }
+
+// item detail page
+exports.itemDetail = function (req, res, next) {
+    Item.findById(req.params.id)
+    .exec(function (err, item) {
+        if (err) {
+            return next(err);
+        }
+        // successful so render
+        res.render('itemDetail', {
+            title: "Tater's General Store",
+            item: item
+        });
+    })
+}
