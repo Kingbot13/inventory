84a1d93f21120c8dd00d7dc1922a56406326359c
Dylan King <dwkblue51@yahoo.com>
2022-09-17 16:29:07 -0500

    add post route for itemForm

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,40 +1,104 @@
-const Item = require('../models/item');
-const {body, validationResult} = require('express-validator');
+const Item = require("../models/item");
+const Category = require("../models/category");
+const { body, validationResult } = require("express-validator");
+const async = require("async");
 
 // get all items
 exports.itemsList = function (req, res, next) {
-    Item.find()
-    .sort([['name', 'ascending']])
+  Item.find()
+    .sort([["name", "ascending"]])
     .exec(function (err, listItems) {
-        if (err) {
-            return next(err);
-        }
-        // successful so render
-        res.render('index', {
-            title: "Tater's General Store",
-            itemsList: listItems
-        });
-    })
-}
+      if (err) {
+        return next(err);
+      }
+      // successful so render
+      res.render("index", {
+        title: "Tater's General Store",
+        itemsList: listItems,
+      });
+    });
+};
 
 // item detail page
 exports.itemDetail = function (req, res, next) {
-    Item.findById(req.params.id)
-    .exec(function (err, item) {
-        if (err) {
-            return next(err);
-        }
-        // successful so render
-        res.render('itemDetail', {
-            title: "Tater's General Store",
-            item: item
-        });
-    })
-}
+  Item.findById(req.params.id).exec(function (err, item) {
+    if (err) {
+      return next(err);
+    }
+    // successful so render
+    res.render("itemDetail", {
+      title: "Tater's General Store",
+      item: item,
+    });
+  });
+};
 
 // display item form
-exports.itemCreateGet = function(req, res, next) {
-    res.render('itemForm', {title: 'Create Item'});
-}
+exports.itemCreateGet = function (req, res, next) {
+  Category.find().exec(function (err, categories) {
+    if (err) {
+      return next(err);
+    }
+    res.render("itemForm", { title: "Create Item", categories: categories });
+  });
+};
 
 // handle item form on post
+exports.itemCreatePost = [
+  // validate and sanitize
+  body("name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Item name must be specified")
+    .isAlphanumeric()
+    .withMessage("Must have alphanumeric characters"),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          items(callback) {
+            Item.find(callback);
+          },
+          categories(callback) {
+            Category.find(callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err);
+          }
+          for (const category of results.categories) {
+            if (item.category.includes(category.name)) {
+              category.checked = "true";
+            }
+          }
+          res.render("itemForm", {
+            title: "Create item",
+            categories: results.categories,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    }
+    item.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(item.url);
+    });
+  },
+];
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const itemController = require('../controllers/itemController');
+const itemController = require("../controllers/itemController");
 
 /* GET home page. */
-router.get('/', itemController.itemsList) 
+router.get("/", itemController.itemsList);
+
+// item form on get
+router.get("/create-item", itemController.itemCreateGet);
+
+// item form on post
+router.post("/create-item", itemController.itemCreatePost);
 
 module.exports = router;
