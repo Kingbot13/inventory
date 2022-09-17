4d274951fa3f729db36c0dd8df06048bec6215ce
Dylan King <dwkblue51@yahoo.com>
2022-09-16 21:11:19 -0500

    add categoryDetail controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,18 +1,44 @@
-const { retry } = require('async');
-const Category = require('../models/category');
+const async = require("async");
+const Category = require("../models/category");
+const Item = require("../models/item");
 
 // get all categories
-exports.categoriesList = function(req, res, next) {
-    Category.find()
-    .sort([['name', 'ascending']])
-    .exec(function(err, list) {
-        if (err) {
-            return next(err);
-        }
-        // successful so render
-        res.render('categoriesList', {
-            title: "Tater's General Store",
-            categoriesList: list
-        });
-    })
-}
+exports.categoriesList = function (req, res, next) {
+  Category.find()
+    .sort([["name", "ascending"]])
+    .exec(function (err, list) {
+      if (err) {
+        return next(err);
+      }
+      // successful so render
+      res.render("categoriesList", {
+        title: "Tater's General Store",
+        categoriesList: list,
+      });
+    });
+};
+
+// get category detail page
+exports.categoryDetail = function (req, res, next) {
+  async.parallel(
+    {
+      category(callback) {
+        Category.find({ name: req.params.category }).exec(callback);
+      },
+      item(callback) {
+        Item.find({ category: req.params.category }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      // successful so render
+      res.render("categoryDetail", {
+        title: "Taters General Store",
+        category: results.category,
+        items: results.item,
+      });
+    }
+  );
+};
--- routes/categories.js
+++ routes/categories.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const category = require('../controllers/categoryController');
 
-router.get('/', )
+router.get('/', category.categoriesList);
+
+
+
+module.exports = router;
